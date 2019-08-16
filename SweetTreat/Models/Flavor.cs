9a173d4a4f8a2eba4ba5983dfce4dc80c1e85fc9
Stephen Crain <rochellevroberts@utexas.edu>
2019-08-16 11:51:54 -0700

    added TreatFlavor join table class

---
+++ SweetTreat/Models/Flavor.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace SweetTreat.Models
+{
+    public class Flavor
+    {
+        public int ID {get; set;}
+        public string Name { get; set;}
+        
+    }
+}

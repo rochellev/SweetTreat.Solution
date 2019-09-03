7c031a41c35bcc5d3f1287cfe79b589ef7c2d2f2
rochellev <rochellevroberts@utexas.edu>
2019-09-03 05:52:03 -0700

    set up account

--- SweetTreat/Migrations/20190903124507_AddIdentity.Designer.cs
+++ SweetTreat/Migrations/20190903124507_AddIdentity.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SweetTreat.Models;
 
 namespace SweetTreat.Migrations
 {
     [DbContext(typeof(SweetTreatContext))]
-    partial class SweetTreatContextModelSnapshot : ModelSnapshot
+    [Migration("20190903124507_AddIdentity")]
+    partial class AddIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
